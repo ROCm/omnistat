1237621bf1f8266e2db109af93a7b91335104044
Jordà Polo <jorda.polo@amd.com>
2025-09-18 17:06:50 -0500

    Parametrize and test both rocm-smi and amd-smi
    Signed-off-by: Jordà Polo <jorda.polo@amd.com>

--- test/test_smi.py
+++ test/test_smi.py
@@ -20,6 +20,14 @@ enable_ras_ecc = False
 rocm_path = /opt/rocm-6.4.1/
 """
 
+CONFIG_AMD_SMI = """
+[omnistat.collectors]
+enable_rocm_smi = False
+enable_amd_smi = False
+enable_network = False
+enable_ras_ecc = False
+"""
+
 SMI_METRICS = [
     "rocm_num_gpus",
     "rocm_version_info",
@@ -40,6 +48,10 @@ class TestMonitor:
     __test__ = False
 
     def __init__(self, config_string):
+        self.address = f"localhost:{test.config.port}"
+        self.url = f"http://{self.address}/metrics"
+        self.timeout = 3.0
+
         config = configparser.ConfigParser()
         config.read_string(config_string)
         monitor = Monitor(config)
@@ -49,21 +61,36 @@ class TestMonitor:
             app.route("/metrics")(lambda: (monitor.updateAllMetrics(), {"Content-Type": "text/plain; charset=utf-8"}))
 
         app = Flask("omnistat")
-        options = {"bind": f"localhost:{test.config.port}", "workers": 1, "post_fork": post_fork}
+        options = {"bind": self.address, "workers": 1, "post_fork": post_fork}
         server = OmnistatServer(app, options)
 
         self._process = multiprocessing.Process(target=server.run)
         self._process.start()
 
-        time.sleep(1)
+        running = self.wait_for_server()
+        if not running:
+            self._process.terminate()
+        assert running is True
 
     def __del__(self):
         self._process.terminate()
 
+    def wait_for_server(self):
+        # Wait until endpoint is up and running, or timeout.
+        start_time = time.time()
+        while time.time() - start_time < self.timeout:
+            try:
+                response = requests.get(self.url)
+                if response.status_code == 200:
+                    return True
+            except requests.RequestException:
+                pass
+            time.sleep(1)
+        return False
+
     def get(self):
         try:
-            url = f"http://localhost:{test.config.port}/metrics"
-            response = requests.get(url)
+            response = requests.get(self.url)
             return text_string_to_metric_families(response.text)
         except requests.exceptions.RequestException as e:
             print(f"Error fetching metrics: {e}")
@@ -71,8 +98,9 @@ class TestMonitor:
 
 class TestSMI:
     @pytest.mark.skipif(not test.config.rocm_host, reason="requires ROCm")
-    def test_smi_init(self):
-        monitor = TestMonitor(CONFIG_ROCM_SMI)
+    @pytest.mark.parametrize("smi_config", [CONFIG_ROCM_SMI,  CONFIG_AMD_SMI])
+    def test_smi_init(self, smi_config):
+        monitor = TestMonitor(smi_config)
         response = monitor.get()
 
         metric_names = {metric.name for metric in response}
