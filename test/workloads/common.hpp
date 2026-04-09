43b6dbcc0c6b2d569bb72f8352396b58044ec5b8
Jordà Polo <jorda.polo@amd.com>
2026-04-09 13:10:42 -0700

    Add common header for workloads
    Signed-off-by: Jordà Polo <jorda.polo@amd.com>

--- test/workloads/common.hpp
+++ test/workloads/common.hpp
@@ -22,39 +22,18 @@
 // IN THE SOFTWARE.
 // ---------------------------------------------------------------------------
 
-// Simple HIP application that launches a configurable number of GPU kernels.
-// Intended for testing kernel tracing with omnistat.
-//
-// Usage: launch_kernels <count> [delay_us]
-//   count    - number of kernel launches
-//   delay_us - microseconds to sleep between launches (default: 0)
+#pragma once
 
-#include "common.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <hip/hip_runtime.h>
 
-#include <unistd.h>
-
-__global__ void empty_kernel() {}
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <count> [delay_us]\n", argv[0]);
-    return 1;
-  }
-
-  int count = atoi(argv[1]);
-  int delay_us = argc > 2 ? atoi(argv[2]) : 0;
-
-  if (count <= 0) {
-    fprintf(stderr, "count must be > 0\n");
-    return 1;
-  }
-
-  for (int i = 0; i < count; i++) {
-    empty_kernel<<<1, 1>>>();
-    if (delay_us > 0)
-      usleep(delay_us);
-  }
-
-  HIP_CHECK(hipDeviceSynchronize());
-  return 0;
-}
+#define HIP_CHECK(call)                                                        \
+  do {                                                                         \
+    hipError_t err = (call);                                                   \
+    if (err != hipSuccess) {                                                   \
+      fprintf(stderr, "HIP error %d: %s at %s:%d\n", err,                     \
+              hipGetErrorString(err), __FILE__, __LINE__);                     \
+      exit(1);                                                                 \
+    }                                                                          \
+  } while (0)
