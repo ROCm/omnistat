7ebd33e8e3559348e9751e4751265644f337d109
Jordà Polo <jorda.polo@amd.com>
2026-07-06 13:42:08 -0700

    Add copyright headers
    Signed-off-by: Jordà Polo <jorda.polo@amd.com>

--- omnistat/inspect/backend/base.py
+++ omnistat/inspect/backend/base.py
@@ -1,3 +1,27 @@
+# -------------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2026 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -------------------------------------------------------------------------------
+
 """DataSource abstraction + query ledger for omnistat-inspect backends.
 
 A ``DataSource`` hides the difference between a Prometheus-compatible TSDB and a
@@ -198,9 +222,7 @@ class DataSource(ABC):
         ``increase()`` (TSDB) override this with a cheaper fast path that is
         bit-for-bit equivalent on monotonic series.
         """
-        results = self.job_query(
-            metric, self.auto_step(), literal_filters=literal_filters, regex_filters=regex_filters
-        )
+        results = self.job_query(metric, self.auto_step(), literal_filters=literal_filters, regex_filters=regex_filters)
         return compute.per_key_increase(results, key_labels)
 
     # -- Abstract API -------------------------------------------------------
